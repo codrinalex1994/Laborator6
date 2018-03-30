499053dc3d239677e0a4442c4380db6bfedef334
alex.mihaes@protonmail.com <alex.mihaes@protonmail.com>
2018-03-30 09:10:16 +0300

    CheckMe PerfectNumber

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -15,4 +15,22 @@ public class PerfectNumber implements Supertip {
     }
 
 
+
+    public boolean checkMe() {
+        int divsorsSum = 0;
+        for (int i = 1; i <= this.value / 2; i++) {
+            if (this.value % i == 0) {
+                divsorsSum += i;
+            }
+        }
+        if (divsorsSum == this.value) {
+            System.out.println("It is a perfect number.");
+            return true;
+        } else {
+            System.out.println("It is not a perfect number.");
+            return false;
+        }
+    }
+
+
 }

404629d5097920e541720e7b4a12a65641868139
Narcis <narcis_diaconu_98@yahoo.com>
2018-03-30 09:31:37 +0300

    Palindrom number

---
+++ test/PalindromNumberTest.groovy
@@ -0,0 +1,15 @@
+class PalindromNumberTest extends GroovyTestCase {
+    void testAdd() {
+    }
+
+    void testMultiply() {
+    }
+
+    void testTestType() {
+    }
+    
+    void testCheckMe() {
+        PalindromNumber p = new PalindromNumber(12321);
+        assertTrue(p.checkMe());
+    }
+}

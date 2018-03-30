a99c7ccb9f166b087e2c8268e82587f04ef175be
codrinalex1994 <codrinalex1994@yahoo.com>
2018-03-30 09:07:52 +0300

    perfNumb

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,2 +1,18 @@
-public class PerfectNumber extends PrimeNumber, Supertip {
+public class PerfectNumber implements Supertip {
+    double value;
+
+    PerfectNumber(int value) {
+        this.value = value;
+    }
+
+    public Supertip add(Supertip s) {
+        this.value += s.value;
+        return this;
+    }
+
+    public Supertip multiply(Supertip s) {
+        return (this.value *= s.value)
+    }
+
+
 }
